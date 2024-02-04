fa8b35824a855e15af9776eedace2f0a72028922
efemerido <l20170734@culiacan.tecnm.mx>
2024-02-03 22:37:35 -0700

    first commit, fetching info about the video from the url is working and a simple download button that also works

---
+++ VideoDownloads/src/App.jsx
@@ -0,0 +1,89 @@
+import { useState, useEffect} from 'react';
+import logo from './assets/logo.jpg'
+import axios from 'axios'
+// import './App.css'
+
+const App =()=> {
+  const [videoUrl, setVideoUrl] = useState('');
+  const [videoInfo, setVideoInfo] = useState(null);
+
+  const submitButton =  async (event) => {
+    event.preventDefault()
+    var urlvideo = videoUrl
+    try {
+      const response = await axios.get(`http://localhost:3001/video-info?url=${urlvideo}`);
+      console.log(response.data);
+      const data = response.data
+      console.log("titulo: ",data.title);
+      setVideoInfo(response.data);
+    } catch (error) {
+      console.error('Error fetching video information:', error);
+    }
+
+  }
+
+  const handleDownload = async () => {
+    try {
+      const response = await axios.post('http://localhost:3001/download', { url: videoUrl }, { responseType: 'blob' });
+      
+      const blob = new Blob([response.data]);
+      const link = document.createElement('a');
+      link.href = window.URL.createObjectURL(blob);
+      link.download = 'downloaded-video.webm';
+      link.click();
+    } catch (error) {
+      console.error('Error initiating video download:', error);
+    }
+  };
+
+
+
+  const handleChangeInput = (event) => {
+    console.log(event.target.value)
+    setVideoUrl(event.target.value)
+  }
+  return (
+    <>
+
+    <header>
+        <img src={logo} className="logo" alt="React logo" />
+        <div className='flexy'>
+        <h1>VixenDL</h1>
+        <p>Download videos or audio from urls</p>
+        </div>
+    </header>
+
+    <div className="card-container">
+    <div className="card">
+      <form onSubmit={submitButton}>
+      <input value={videoUrl} type="text"  placeholder="Escribe la URL aquí" 
+      onChange={handleChangeInput}/>
+      
+      <button type='submit'>Buscar</button>
+      
+      </form>
+      {
+      videoInfo !=null ? 
+      (
+         <div className='videoInfo'>
+         <p>{videoInfo.title}</p>
+         <img src={videoInfo.thumbnail} alt="" />
+         <div className='butdur'>
+           <p>duration: {videoInfo.duration}</p> 
+           <button onClick={handleDownload} className='download'>Download</button>
+         </div>
+       </div>
+      ) 
+      : null
+      }
+    </div>
+  </div>
+
+    <footer>
+      <p>hecho por el god</p>
+    </footer>
+ </>
+  )
+}
+
+export default App
---
+++ serverside/index.js
@@ -0,0 +1,83 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const { spawn } = require('child_process');
+const path = require('path');
+
+
+app.use(express.json()); 
+
+app.use(cors())
+
+app.get('/video-info', (request, response) => {
+  const videoUrl = request.query.url;
+  let dataBuffer = '';
+
+  // sub-proceso para buscar la informacion del URL
+  const DlProcess = spawn('yt-dlp', ['--dump-json', videoUrl]);
+
+  DlProcess.stdout.on('data', (data) => {
+      // acumula todo en un string para despues parsearlo
+      dataBuffer += data;
+      console.log(`yt-dlp stdout: ${data}`);
+  });
+
+  DlProcess.stderr.on('data', (data) => {
+      console.error(`yt-dlp stderr: ${data}`);
+  });
+
+  DlProcess.on('close', (code) => {
+      if (code === 0) {
+          try {
+              // Parsea la wea 
+              const jsonData = JSON.parse(dataBuffer);
+              response.json(jsonData);
+          } catch (error) {
+              console.error('Error parsing JSON:', error);
+              response.status(500).json({ error: 'Internal Server Error' });
+          }
+      } else {
+          console.error(`yt-dlp process exited with code ${code}`);
+          response.status(500).json({ error: 'Internal Server Error' });
+      }
+  });
+});
+  
+app.post('/download', (req, res) => {
+  const videoUrl = req.body.url;
+
+  // sub-proceso para ejecutar los comandos en la consola
+  const ytDlpProcess = spawn('yt-dlp', [
+    '-o', path.join(__dirname, 'VideosDescargados', 'my_video.webm'),
+    videoUrl
+  ]);
+  ytDlpProcess.stdout.on('data', (data) => {
+    // los resultados imprimir a consola
+    console.log(`yt-dlp stdout: ${data}`);
+  });
+
+  ytDlpProcess.stderr.on('data', (data) => {
+    // por si hay algun error durante la ejecucion 
+    console.error(`yt-dlp stderr: ${data}`);
+  });
+
+  ytDlpProcess.on('close', (code) => {
+    if (code === 0) {
+      // video descargado de forma exitosa
+      // Enviar el video al cliente
+      const videoFilePath = path.join(__dirname, 'VideosDescargados', 'my_video.webm');
+      res.sendFile(videoFilePath);
+    } else {
+      // La descarga falló
+      res.status(500).json({ error: 'Video download failed' });
+    }
+  });
+});
+
+
+
+
+  const PORT = 3001
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
